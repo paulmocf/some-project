5591aaed02899e7a63e7795455e9888127890ffe
Paul Mocanu <paul.mocanu@stsnet.ro>
2025-04-03 19:17:09 +0300

    added theme functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import {ThemeToggler} from "@/app/components/ThemeToggler";
+
 export default function Home() {
     return (
-        <></>
+        <>
+            <ThemeToggler/>
+        </>
     );
 }
