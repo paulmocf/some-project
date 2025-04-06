0fb341263c7321ac1c1d2306cfb68f2494599446
Paul Mocanu <paul.mocanu@stsnet.ro>
2025-04-06 17:56:38 +0300

    reverted to first toggler

--- src/app/components/ThemeToggler.tsx
+++ src/app/components/ThemeToggler.tsx
@@ -10,15 +10,12 @@ export function ThemeToggler() {
     const {theme, setTheme} = useTheme()
 
     return (
-        <Button variant="ghost" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-            {theme === 'dark' ? <><Sun
-                    className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-                    <span className="text-sm flex md:hidden">Light Mode</span> </>
-                : <><Moon
-                    className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
-                    <span className="text-sm flex md:hidden">Dark Mode</span>
-                </>
-            }
+        <Button variant="outline" size="icon"
+                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            <Sun
+                className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+            <Moon
+                className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0"/>
             <span className="sr-only">Toggle theme</span>
         </Button>
     )
